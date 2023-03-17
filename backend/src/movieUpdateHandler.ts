bdf65fb679ce463486b296a5a35dc21877bc8f1a
Rafik <rafiksargsyan07@gmail.com>
2023-03-17 23:55:12 +0100

    Added algolia update logic

--- backend/src/movieUpdateHandler.ts
+++ backend/src/movieUpdateHandler.ts
@@ -1,12 +1,15 @@
 import { Movie } from "./Movie";
 import { Marshaller } from '@aws/dynamodb-auto-marshaller';
-import { DynamoDBStreamEvent } from "aws-lambda";
+import { DynamoDBStreamEvent } from 'aws-lambda';
+import algoliasearch from 'algoliasearch'; 
 
 const marshaller = new Marshaller();
+const algoliaClient = algoliasearch(process.env.ALGOLIA_APP_ID!, process.env.ALGOLIA_ADMIN_KEY!);
+const algoliaIndex = algoliaClient.initIndex(process.env.ALGOLIA_MOVIE_INDEX!);
 
 export const handler = async (event: DynamoDBStreamEvent): Promise<void> => {
   for (const record of event.Records) {
     let movie: Movie = marshaller.unmarshallItem(record.dynamodb?.NewImage!) as unknown as Movie;
-    // update algolia;
+    algoliaIndex.saveObject(movie);
   }
 };
