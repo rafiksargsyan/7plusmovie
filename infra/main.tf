1ac8a54d7794977f75434792e8de88bed23477cf
Rafik <rafiksargsyan07@gmail.com>
2023-03-20 01:57:51 +0100

    Added rule for intelligent tiering

--- infra/main.tf
+++ infra/main.tf
@@ -11,3 +11,15 @@ locals {
 resource "aws_s3_bucket" "media_assets" {
     bucket = "media-assets-${local.deployment_id}"
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "media_assets_lifecycle_config" {
+  bucket = aws_s3_bucket.media_assets.bucket
+
+  rule {
+    id = "intelligent-tiering"
+
+    transition {
+        storage_class = "INTELLIGENT_TIERING"
+    }
+  }
+}
