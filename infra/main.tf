9d164bd6bd30fa2db00416f6b9177ec2cb30f850
Rafik <rafiksargsyan07@gmail.com>
2023-03-20 01:29:34 +0100

    Fixed liefcycle config

--- infra/main.tf
+++ infra/main.tf
@@ -2,7 +2,10 @@ resource "aws_s3_bucket" "media_assets" {
     bucket = "media-assets"
 }
 
-resource "aws_s3_bucket_intelligent_tiering_configuration" "media_assets_it_config" {
-    bucket = aws_s3_bucket.media_assets.id
-    name   = "media-assets-it-config"
+resource "aws_s3_bucket_lifecycle_configuration" "media_assets_lifecycle_config" {
+    bucket = aws_s3_bucket.media_assets.bucket
+    
+    transition {
+        storage_class = "INTELLIGENT_TIERING"
+    }
 }
