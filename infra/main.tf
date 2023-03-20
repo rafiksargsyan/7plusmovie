eaf0d307ed5652d892f2b0489f7eb060794e8b5b
Rafik <rafiksargsyan07@gmail.com>
2023-03-20 01:33:26 +0100

    Remove lifecycle config

--- infra/main.tf
+++ infra/main.tf
@@ -1,11 +1,3 @@
 resource "aws_s3_bucket" "media_assets" {
     bucket = "media-assets"
 }
-
-resource "aws_s3_bucket_lifecycle_configuration" "media_assets_lifecycle_config" {
-    bucket = aws_s3_bucket.media_assets.bucket
-    
-    transition {
-        storage_class = "INTELLIGENT_TIERING"
-    }
-}
