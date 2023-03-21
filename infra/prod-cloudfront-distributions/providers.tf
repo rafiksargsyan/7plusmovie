2c4673d3ac9b815021590e58155954ba24b69fae
Rafik <rafiksargsyan07@gmail.com>
2023-03-22 00:30:21 +0100

    Fixed provider credentials

--- infra/prod-cloudfront-distributions/providers.tf
+++ infra/prod-cloudfront-distributions/providers.tf
@@ -6,7 +6,11 @@ provider "aws" {
 
 provider "aws" {
   alias  = "chalkhalting"
+
   region = "eu-west-3"
+  access_key = var.aws_workloads_prod_access_key
+  secret_key = var.aws_workloads_prod_secret_key
+
   assume_role {
     role_arn     = "arn:aws:iam::863389750426:role/WorkloadsProd1AssumeRole"
     session_name = "chalkhalting"
