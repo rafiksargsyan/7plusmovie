520c7722771882d57f03ce1b064a0a2587861b54
Rafik <rafiksargsyan07@gmail.com>
2023-03-22 00:41:19 +0100

    Fixed forwarded value problem

--- infra/prod-cloudfront-distributions/main.tf
+++ infra/prod-cloudfront-distributions/main.tf
@@ -10,6 +10,13 @@ resource "aws_cloudfront_distribution" "chalkhalting" {
     cached_methods         = []
     target_origin_id       = data.aws_s3_bucket.media_assets_bucket.bucket
     viewer_protocol_policy = "allow-all"
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
   }
   origin {
     domain_name = data.aws_s3_bucket.media_assets_bucket.bucket_regional_domain_name
