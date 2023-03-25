9eeaa41bd01a59e5494b501d64b327fb8fab0a8c
Rafik <rafiksargsyan07@gmail.com>
2023-03-25 19:53:04 +0100

    renamed backend to services

--- backend/src/AudioLangCodes.ts
+++
@@ -1,6 +0,0 @@
-export const AudioLangCodes = {
-  EN : { lang : "en", langTag : "en" },
-  EN_US : { lang : "en", langTag : "en-US" },
-  EN_GB : { lang : "en", langTag : "en-GB" },
-  RU : { lang : "ru", langTag : "ru" }    
-} as const;
--- backend/src/SubsLangCodes.ts
+++
@@ -1,7 +0,0 @@
-export const SubsLangCodes = {
-  EN : { lang : "en", langTag : "en" },
-  EN_US : { lang : "en", langTag : "en-US" },
-  EN_GB : { lang : "en", langTag : "en-GB" },
-  RU : { lang : "ru", langTag : "ru" }    
-} as const;
-
