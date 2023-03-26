d0b95a0413a09415958af2bbf56067f180ebd03b
Rafik <rafiksargsyan07@gmail.com>
2023-03-26 01:53:45 +0100

    minor refactoring

--- infra/main/output.tf
+++ infra/main/output.tf
@@ -17,3 +17,7 @@ output "ddb_cf_distro_metadata_table_name" {
 output "secret_manager_secret_id" {
   value = aws_secretsmanager_secret.secrets.name
 }
+
+output "media_assets_s3_bucket_name" {
+  value = aws_s3_bucket.media_assets.bucket
+}
