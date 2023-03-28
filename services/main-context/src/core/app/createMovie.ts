2020776bd00908e94bb3041b7dcd7b43a27dacd9
Rafik <rafiksargsyan07@gmail.com>
2023-03-29 01:20:22 +0200

    Some env and other fixes )

--- services/main-context/src/core/app/createMovie.ts
+++ services/main-context/src/core/app/createMovie.ts
@@ -5,7 +5,13 @@ import { L8nLangCode } from "../domain/L8nLangCodes";
 
 const dynamodbMovieTableName = process.env.DYNAMODB_MOVIE_TABLE_NAME!;
 
-const docClient = DynamoDBDocument.from(new DynamoDB({}));
+const marshallOptions = {
+  convertClassInstanceToMap: true
+};
+
+const translateConfig = { marshallOptions };
+
+const docClient = DynamoDBDocument.from(new DynamoDB({}), translateConfig);
 
 interface CreateMovieParam {
   originalLocale: string;
@@ -18,7 +24,6 @@ export const handler = async (event: CreateMovieParam): Promise<string> => {
   //       there have been few cases when two movies with same title had been released
   //       in the same year, but most likely we will not have such case with our movies
   let movie = new Movie(false, new L8nLangCode(event.originalLocale), event.originalTitle, event.releaseYear);
-
   await docClient.put({TableName: dynamodbMovieTableName, Item: movie});
 
   return movie.id;
