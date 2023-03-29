719fc7d331e29391a3ef2b8412dd7fb689a1d1d5
Rafik <rafiksargsyan07@gmail.com>
2023-03-29 02:52:42 +0200

    Added unique check useing algolia search

--- services/main-context/src/core/app/createMovie.ts
+++ services/main-context/src/core/app/createMovie.ts
@@ -2,6 +2,7 @@ import { Movie } from "../domain/Movie";
 import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
 import { DynamoDB } from '@aws-sdk/client-dynamodb';
 import { L8nLangCode } from "../domain/L8nLangCodes";
+import algoliasearch from 'algoliasearch/lite';
 
 const dynamodbMovieTableName = process.env.DYNAMODB_MOVIE_TABLE_NAME!;
 
@@ -13,6 +14,9 @@ const translateConfig = { marshallOptions };
 
 const docClient = DynamoDBDocument.from(new DynamoDB({}), translateConfig);
 
+const algoliaSearchOnlyClient = algoliasearch(process.env.ALGOLIA_APP_ID!, process.env.ALGOLIA_SEARCH_ONLY_KEY!);
+const algoliaIndex = algoliaSearchOnlyClient.initIndex(process.env.ALGOLIA_ALL_INDEX!);
+
 interface CreateMovieParam {
   originalLocale: string;
   originalTitle: string;
@@ -22,9 +26,22 @@ interface CreateMovieParam {
 export const handler = async (event: CreateMovieParam): Promise<string> => {
   // TODO: Check if a movie with the same title and year already exists. Actually
   //       there have been few cases when two movies with same title had been released
-  //       in the same year, but most likely we will not have such case with our movies
+  //       in the same year, but most likely we will not have such case with our movies.
+  //       Currently just looking in Algolia. Yeah, now I miss about SQL ).
   let movie = new Movie(false, new L8nLangCode(event.originalLocale), event.originalTitle, event.releaseYear);
+  algoliaIndex.search(event.originalTitle).then(({ hits }) => {
+    hits.forEach((x) => {
+      const xOriginalTitle = x['originalTitle'] as string;
+      const xReleaseYear = x['releaseYear'] as number;
+      if (xOriginalTitle.toUpperCase() === event.originalTitle.toUpperCase() &&
+        xReleaseYear === event.releaseYear) {
+          throw new MovieAlreadyExistsError();
+      }
+    })
+  })
   await docClient.put({TableName: dynamodbMovieTableName, Item: movie});
 
   return movie.id;
 };
+
+class MovieAlreadyExistsError extends Error {};
