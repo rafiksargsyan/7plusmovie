ad6fea85d9ee461e0784d8d65f81dae1e2ef98d4
Rafik Sargsyan <rafik.sargsyan@spark.net>
2023-04-18 23:55:43 +0200

    Allow user/password auth

--- infra/main/main.tf
+++ infra/main/main.tf
@@ -139,12 +139,14 @@ resource "aws_cognito_user_pool" "admin_user_pool" {
 resource "aws_cognito_user_pool_client" "admin_client_cli" {
   name         = "${local.deployment_id}-admin-client-cli"
   user_pool_id = aws_cognito_user_pool.admin_user_pool.id
+
+  allowed_oauth_flows = ["USER_PASSWORD_AUTH"]
 }
 
 resource "aws_cognito_user" "rafiksargsyan07" {
   user_pool_id             = aws_cognito_user_pool.admin_user_pool.id
   username                 = "rafiksargsyan07@gmail.com"
-  desired_delivery_mediums = [ "EMAIL" ]
+  desired_delivery_mediums = ["EMAIL"]
   attributes = {
     email = "rafiksargsyan07@gmail.com"
   }
