bdeb9c40f89c536be44a0544dee6736beb8d07ef
Rafik Sargsyan <rafik.sargsyan@spark.net>
2023-04-24 22:29:29 +0200

    Added favicon

--- web-ui/pages/index.tsx
+++ web-ui/pages/index.tsx
@@ -29,6 +29,10 @@ function CatalogPage() {
     <> 
       <Head>
         <title>{L8nTable[currentLocaleCode]['TITLE']}</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
       <Catalog />
     </>
