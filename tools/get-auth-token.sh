5ac5ada94c125a14ebd4cd9bb347c73ac1450129
Rafik Sargsyan <rafik.sargsyan@spark.net>
2023-04-24 21:45:32 +0200

    Added util script to get auth token, Added title to catalog page

---
+++ tools/get-auth-token.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+  echo "Usage: $(basename \$0) <CLIENT_ID> <USERNAME> <PASSWORD>"
+  echo "Prod cli client id: 2so94a4hnjq3hb38a3km1huf9d"
+  exit 0
+fi
+
+
+aws cognito-idp initiate-auth --auth-flow USER_PASSWORD_AUTH --client-id $1 --auth-parameters "USERNAME=${2},PASSWORD=${3}"
+
--- web-ui/pages/index.tsx
+++ web-ui/pages/index.tsx
@@ -1,5 +1,38 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import Catalog from '../components/Catalog';
 
-const catalog = () => <Catalog />
+const L8nLangCodes = {
+  EN_US : { langTag : "en-US", countryCode: "US" },
+  RU : { langTag : "ru", countryCode: "RU" }
+} as const;
+  
+const langTagToLangCode = {
+  "en-US" : "EN_US",
+  "ru" : "RU"
+} as const;
 
-export default catalog
+const L8nTable = {
+  EN_US : {
+    TITLE: "Watch Free Movies Online"
+  },
+  RU : {
+    TITLE: "Смотри бесплатные фильмы онлайн"
+  }
+}
+
+function CatalogPage() {
+  const router = useRouter();
+  const currentLocale = router.locale != null ? router.locale : router.defaultLocale!;
+  const currentLocaleCode = langTagToLangCode[currentLocale as keyof typeof langTagToLangCode];
+  return (
+    <> 
+      <Head>
+        <title>{L8nTable[currentLocaleCode]['TITLE']}</title>
+      </Head>
+      <Catalog />
+    </>
+  )  
+}
+
+export default CatalogPage;
