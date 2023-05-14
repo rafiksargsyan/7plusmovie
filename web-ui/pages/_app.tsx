06ac463cef0c6738ae1fa3c479e4f34cf9bc218e
Rafik Sargsyan <rafik.sargsyan@spark.net>
2023-05-14 12:00:19 +0200

    Fixed route change loading problem

--- web-ui/pages/_app.tsx
+++ web-ui/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { Box, CircularProgress, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import { red } from '@mui/material/colors';
 import type { AppProps } from 'next/app'
-import { Router } from 'next/router';
+import { useRouter } from 'next/router';
 import Script from 'next/script';
 import { useEffect, useState } from 'react';
 import '../components/Player.css'
@@ -17,22 +17,33 @@ const darkTheme = createTheme({
 
 export default function App({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
   useEffect(() => {
-    const start = () => {
+    router.beforePopState(() => {
       setLoading(true);
+      return true;
+    });
+    const onRouteChangeStart = (url: string) => {
+      if (url.includes("/player?")) {
+        setLoading(true);
+      }
     };
     const end = () => {
-      setLoading(false);
+      if (loading === true) {
+        setLoading(false);
+      }
     };
-    Router.events.on("routeChangeStart", start);
-    Router.events.on("routeChangeComplete", end);
-    Router.events.on("routeChangeError", end);
+    router.events.on("routeChangeStart", onRouteChangeStart);
+    router.events.on("routeChangeComplete", end);
+    router.events.on("routeChangeError", end);
     return () => {
-      Router.events.off("routeChangeStart", start);
-      Router.events.off("routeChangeComplete", end);
-      Router.events.off("routeChangeError", end);
+      router.events.off("routeChangeStart", onRouteChangeStart);
+      router.events.off("routeChangeComplete", end);
+      router.events.off("routeChangeError", end);
     };
-  }, []);
+  }, [router]);
+
   return (
     <>
       <Script
