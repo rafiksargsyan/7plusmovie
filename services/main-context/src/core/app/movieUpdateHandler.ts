df5c78357d6d9fea0d27c68713fc01cb29a833f0
Rafik Sargsyan <rafik.sargsyan@spark.net>
2023-06-17 10:57:34 +0200

    Delete cloudinary, algolia and s3 resources when, movie is deleted

--- services/main-context/src/core/app/movieUpdateHandler.ts
+++ services/main-context/src/core/app/movieUpdateHandler.ts
@@ -2,10 +2,13 @@ import { Marshaller } from '@aws/dynamodb-auto-marshaller';
 import { DynamoDBStreamEvent } from 'aws-lambda';
 import algoliasearch from 'algoliasearch';
 import { SecretsManager } from '@aws-sdk/client-secrets-manager';
+import { v2 as cloudinary } from 'cloudinary';
+import { S3 } from '@aws-sdk/client-s3';
 
 const secretManagerSecretId = process.env.SECRET_MANAGER_SECRETS_ID!;
 
 const secretsManager = new SecretsManager({});
+const s3 = new S3({});
 
 const marshaller = new Marshaller();
 
@@ -26,10 +29,16 @@ export const handler = async (event: DynamoDBStreamEvent): Promise<void> => {
   const secret = JSON.parse(secretStr.SecretString!);
   const algoliaClient = algoliasearch(process.env.ALGOLIA_APP_ID!, secret.ALGOLIA_ADMIN_KEY!);
   const algoliaIndex = algoliaClient.initIndex(process.env.ALGOLIA_ALL_INDEX!);
-  
+  cloudinary.config({ cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+                      api_key: process.env.CLOUDINARY_API_KEY,
+                      api_secret: secret.CLOUDINARY_API_SECRET });
+
   for (const record of event.Records) {
     if (record.eventName === 'REMOVE') {
-      await algoliaIndex.deleteBy({filters: `objectID: ${record.dynamodb?.Keys?.id.S}`});
+      let objectID = record.dynamodb?.Keys?.id.S;
+      await algoliaIndex.deleteBy({filters: `objectID: ${objectID}`});
+      await cloudinary.api.delete_resources_by_prefix(objectID!);
+      await emptyS3Directory(process.env.MEDIA_ASSETS_S3_BUCKET, `${objectID}/`);
     } else {
       let movie: Movie = marshaller.unmarshallItem(record.dynamodb?.NewImage!) as unknown as Movie;
       if (movie.mpdFile == null || movie.m3u8File == null
@@ -46,3 +55,28 @@ export const handler = async (event: DynamoDBStreamEvent): Promise<void> => {
     }
   }
 };
+
+async function emptyS3Directory(bucket, dir) {
+  const listParams = {
+      Bucket: bucket,
+      Prefix: dir
+  };
+
+  const listedObjects = await s3.listObjectsV2(listParams);
+
+  if (listedObjects.Contents == undefined ||
+    listedObjects.Contents.length === 0) return;
+
+  const deleteParams = {
+      Bucket: bucket,
+      Delete: { Objects: [] }
+  };
+
+  listedObjects.Contents.forEach(({ Key }) => {
+      deleteParams.Delete.Objects.push({ Key } as never);
+  });
+
+  await s3.deleteObjects(deleteParams);
+
+  if (listedObjects.IsTruncated) await emptyS3Directory(bucket, dir);
+}
