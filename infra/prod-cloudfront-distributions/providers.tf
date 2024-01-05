7a3587a712cc4c06ee7d57b151b8e97d0fb19e6d
Rafik Sargsyan <rafiksargsyan@gmail.com>
2024-01-05 15:00:37 +0400

    removed unused providers

--- infra/prod-cloudfront-distributions/providers.tf
+++ infra/prod-cloudfront-distributions/providers.tf
@@ -18,32 +18,6 @@ provider "aws" {
 }
 
 provider "aws" {
-  alias  = "chalkhalting-1"
-
-  region = "eu-west-3"
-  access_key = var.aws_workloads_prod_access_key
-  secret_key = var.aws_workloads_prod_secret_key
-
-  assume_role {
-    role_arn     = "arn:aws:iam::139486306394:role/WorkloadsProd1AssumeRole"
-    session_name = "chalkhalting1"
-  }
-}
-
-provider "aws" {
-  alias  = "chalkhalting-2"
-
-  region = "eu-west-3"
-  access_key = var.aws_workloads_prod_access_key
-  secret_key = var.aws_workloads_prod_secret_key
-
-  assume_role {
-    role_arn     = "arn:aws:iam::894612417045:role/WorkloadsProd1AssumeRole"
-    session_name = "chalkhalting2"
-  }
-}
-
-provider "aws" {
   alias  = "mutantcandlelight"
 
   region = "eu-west-3"
@@ -57,32 +31,6 @@ provider "aws" {
 }
 
 provider "aws" {
-  alias  = "mutantcandlelight_1"
-
-  region = "eu-west-3"
-  access_key = var.aws_workloads_prod_access_key
-  secret_key = var.aws_workloads_prod_secret_key
-
-  assume_role {
-    role_arn     = "arn:aws:iam::011949300477:role/WorkloadsProd1AssumeRole"
-    session_name = "mutantcandlelight1"
-  }
-}
-
-provider "aws" {
-  alias  = "mutantcandlelight_2"
-
-  region = "eu-west-3"
-  access_key = var.aws_workloads_prod_access_key
-  secret_key = var.aws_workloads_prod_secret_key
-
-  assume_role {
-    role_arn     = "arn:aws:iam::386464960221:role/WorkloadsProd1AssumeRole"
-    session_name = "mutantcandlelight2"
-  }
-}
-
-provider "aws" {
   alias  = "tracenoon"
 
   region = "eu-west-3"
@@ -92,31 +40,5 @@ provider "aws" {
   assume_role {
     role_arn     = "arn:aws:iam::694508858683:role/WorkloadsProd1AssumeRole"
     session_name = "tracenoon"
-  }
-}
-
-provider "aws" {
-  alias  = "tracenoon_1"
-
-  region = "eu-west-3"
-  access_key = var.aws_workloads_prod_access_key
-  secret_key = var.aws_workloads_prod_secret_key
-
-  assume_role {
-    role_arn     = "arn:aws:iam::831916282287:role/WorkloadsProd1AssumeRole"
-    session_name = "tracenoon_1"
-  }
-}
-
-provider "aws" {
-  alias  = "tracenoon_2"
-
-  region = "eu-west-3"
-  access_key = var.aws_workloads_prod_access_key
-  secret_key = var.aws_workloads_prod_secret_key
-
-  assume_role {
-    role_arn     = "arn:aws:iam::855101825283:role/WorkloadsProd1AssumeRole"
-    session_name = "tracenoon_2"
   }
 }
