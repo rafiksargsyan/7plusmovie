246e8046d2d01225fe6c7c99587574b6c1d830f9
Rafik Sargsyan <rafiksargsyan07@gmail.com>
2024-03-07 13:06:16 +0100

    Improved episodes update performance

--- services/main-context/src/core/app/tv-show/addEpisodesBasedOnTmdbSeasonNumber.ts
+++ services/main-context/src/core/app/tv-show/addEpisodesBasedOnTmdbSeasonNumber.ts
@@ -2,8 +2,6 @@ import { SecretsManager } from '@aws-sdk/client-secrets-manager';
 import { DynamoDB } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocument} from '@aws-sdk/lib-dynamodb';
 import axios from 'axios';
-import { handler as addEpisodeToTvShow } from './addEpisodeToTvShow';
-import { handler as addTmdbEpisodeNumber } from "./addTmdbEpisodeNumber";
 import { TvShowRepositoryInterface } from '../../ports/TvShowRepositoryInterface';
 import { TvShowRepository } from '../../../adapters/TvShowRepository';
 
@@ -49,12 +47,13 @@ export const handler = async (event: Param): Promise<void> => {
   const secretStr = await secretsManager.getSecretValue({ SecretId: secretManagerSecretId});
   const secret = JSON.parse(secretStr.SecretString!);  
   const tmdbApiKey = secret.TMDB_API_KEY!;
+ 
+  let tvShow = await tvShowRepo.getTvShowById(event.tvShowId);
+  let tvShowRead: TvShowRead = tvShow as unknown as TvShowRead;
 
-  let tvShow: TvShowRead = await tvShowRepo.getTvShowById(event.tvShowId) as unknown as TvShowRead;
+  const season = tvShowRead.seasons.filter(_ => _.seasonNumber === event.seasonNumber)[0];
 
-  const season = tvShow.seasons.filter(_ => _.seasonNumber === event.seasonNumber)[0];
-
-  if (tvShow.tmdbId == undefined) {
+  if (tvShowRead.tmdbId == undefined) {
     throw new MissingTmdbIdError();
   }
 
@@ -66,20 +65,20 @@ export const handler = async (event: Param): Promise<void> => {
    throw new MissingTmdbSeasonNumberError();
   }
 
-  const tmdbSeason = (await tmdbClient.get(`tv/${tvShow.tmdbId}/season/${season.tmdbSeasonNumber}?api_key=${tmdbApiKey}`)).data;
+  const tmdbSeason = (await tmdbClient.get(`tv/${tvShowRead.tmdbId}/season/${season.tmdbSeasonNumber}?api_key=${tmdbApiKey}`)).data;
 
   for (const tmdbEpisode of tmdbSeason.episodes) {
     const tmdbEpisodeNumber = tmdbEpisode.episode_number;
     const tmdbEpisodeName = tmdbEpisode.name;
     try {
-      await addEpisodeToTvShow({tvShowId: event.tvShowId, originalName: tmdbEpisodeName,
-                                seasonNumber: event.seasonNumber, episodeNumber: tmdbEpisodeNumber});
-      await addTmdbEpisodeNumber({tvShowId: event.tvShowId, tmdbEpisodeNumber: tmdbEpisodeNumber,
-                                  seasonNumber: event.seasonNumber, episodeNumber: tmdbEpisodeNumber});
+      tvShow.addEpisodeToSeason(event.seasonNumber, tmdbEpisodeName, tmdbEpisodeNumber);
+      tvShow.addTmdbEpisodeNumber(event.seasonNumber, tmdbEpisodeNumber, tmdbEpisodeNumber);
     } catch (e) {
       console.error(e);
     }
   }
+
+  await tvShowRepo.saveTvShow(tvShow);
 }
 
 class SeasonNotFoundError extends Error {}
