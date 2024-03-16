61b0d8c1417354b17fa4833fe6487093d684e475
Rafik Sargsyan <rafiksargsyan07@gmail.com>
2024-03-16 16:59:11 +0100

    initial commit

--- web-ui/pages/[[...index]].tsx
+++ web-ui/pages/[[...index]].tsx
@@ -2,7 +2,7 @@ import algoliasearch from 'algoliasearch';
 import { GetServerSidePropsContext } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Catalog from '../components/Catalog';
 
 const L8nLangCodes = {
@@ -26,6 +26,33 @@ const L8nTable = {
   }
 }
 
+const PAGE_SIZE = 40;
+
+const getPage  = async (pageNumber: number, search: string | undefined) => {
+  const algoliaClient = algoliasearch(process.env.NEXT_PUBLIC_ALGOLIA_APP_ID!,
+    process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_ONLY_KEY!);
+  const algoliaIndex = algoliaClient.initIndex(process.env.NEXT_PUBLIC_ALGOLIA_ALL_INDEX!);
+  const algoliaSearchResponse = await algoliaIndex.search<AlgoliaQueryItem>(search == null ? '' : search,
+    {hitsPerPage: 1000});
+  const numOfHits = algoliaSearchResponse.hits.length;
+  return {
+    movies: algoliaSearchResponse.hits.sort((a, b) => {
+      if (a.releaseYear !== b.releaseYear) {
+        return b.releaseYear - a.releaseYear;
+      }
+      return b.creationTime - a.creationTime;
+      }).slice(pageNumber * PAGE_SIZE, pageNumber * PAGE_SIZE + PAGE_SIZE).map(_ => ({
+        id: _.objectID,
+        originalTitle: _.originalTitle,
+        titleL8ns: _.titleL8ns,
+        releaseYear: _.releaseYear,
+        posterImagesPortrait: _.posterImagesPortrait,
+        category: _.category})),
+    pageNumber: pageNumber,
+    numOfPages: Math.ceil(numOfHits / PAGE_SIZE)
+  }  
+};
+
 interface MovieItem {
   id: string;
   originalTitle: string;
@@ -39,23 +66,59 @@ interface CatalogPageProps {
   movies: MovieItem[];
   searchString: string;
   currentLocale: keyof typeof L8nLangCodes;
+  pageNumber: number;
+  numOfPages: number;
 }
 
 function CatalogPage(props: CatalogPageProps) {
   const router = useRouter();
   const [locale, setLocale] = useState(props.currentLocale);
   const [search, setSearch] = useState(props.searchString);
+  const [items, setItems] = useState(props.movies);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(0);
+
+  const fetchMoreData = async () => {
+    if (isLoading) return;
+    setIsLoading(true);
+    const newData = await getPage(page + 1, search);
+    setItems(items => [...items, ...newData.movies]);
+    setPage(prevPage => prevPage + 1);
+    setIsLoading(false);
+  };
 
   const onSearchChange = (searchString: string | null) => {
     router.replace({ pathname: router.basePath, query: {search: searchString}});
     setSearch(searchString != null ? searchString : '');
+    setPage(0);
+    getPage(0, searchString === null ? undefined : searchString).then((data) => {
+      setItems(data.movies);
+    });
   };
   
   const onLocaleChange = (locale: string) => {
     router.replace(router.asPath, undefined, { locale: L8nLangCodes[locale as keyof typeof L8nLangCodes].langTag });
     setLocale(locale as keyof typeof L8nLangCodes);
   }
-  
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const {scrollHeight, clientHeight, scrollTop} = {...document.documentElement};
+      if (
+        scrollHeight > clientHeight + scrollTop || isLoading
+      ) {
+        return;
+      }
+      fetchMoreData();
+    };
+    
+    document.addEventListener('scroll', handleScroll);
+
+    return () => {
+      document.removeEventListener('scroll', handleScroll);
+    };
+  }, [isLoading, page, search, fetchMoreData]);
+
   return (
     <>
       <Head>
@@ -71,9 +134,12 @@ function CatalogPage(props: CatalogPageProps) {
         <link rel="alternate" href="https://www.q62.xyz/en-US" hrefLang='en-US'></link>
         <link rel="alternate" href="https://www.q62.xyz/ru" hrefLang='ru'></link>
         <link rel="alternate" href="https://www.q62.xyz/en-US" hrefLang='x-default'></link>
+        {props.pageNumber > 0 && <link rel="prev" href={`/${L8nLangCodes[locale as keyof typeof L8nLangCodes].langTag}/?page=${props.pageNumber-1}`}></link>}
+        {props.pageNumber + 1 < props.numOfPages && <link rel="next" href={`/${L8nLangCodes[locale as keyof typeof L8nLangCodes].langTag}/?page=${props.pageNumber+1}`}></link>}
       </Head>
-      <Catalog {...props} currentLocale={locale} searchString={search}
+      <Catalog {...props} movies={items} currentLocale={locale} searchString={search}
         onSearchChange={onSearchChange} onLocaleChange={onLocaleChange}/>
+      {isLoading && <div>Loading...</div>}  
     </>
   )  
 }
@@ -91,29 +157,17 @@ interface AlgoliaQueryItem {
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const algoliaClient = algoliasearch(process.env.NEXT_PUBLIC_ALGOLIA_APP_ID!,
-    process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_ONLY_KEY!);
-  const algoliaIndex = algoliaClient.initIndex(process.env.NEXT_PUBLIC_ALGOLIA_ALL_INDEX!);
   const searchString = context.query.search as (string | undefined);
-  const algoliaSearchResponse = await algoliaIndex.search<AlgoliaQueryItem>(searchString == null ? '' : searchString,
-    {hitsPerPage: 1000});
+  let pageNumber =  Number(context.query.page);
+  if (Number.isNaN(pageNumber)) {
+    pageNumber = 0;
+  }
   const locale = (context.locale != null ? context.locale : context.defaultLocale!) as keyof typeof langTagToLangCode;
   const langCode = langTagToLangCode[locale];
   return { props: {
-    movies: algoliaSearchResponse.hits.sort((a, b) => {
-      if (a.releaseYear !== b.releaseYear) {
-        return b.releaseYear - a.releaseYear;
-      }
-      return b.creationTime - a.creationTime;
-  }).map(_ => ({
-      id: _.objectID,
-      originalTitle: _.originalTitle,
-      titleL8ns: _.titleL8ns,
-      releaseYear: _.releaseYear,
-      posterImagesPortrait: _.posterImagesPortrait,
-      category: _.category})),
-      currentLocale: langCode, 
-      searchString: searchString
+      currentLocale: langCode,
+      searchString: searchString,
+      ...(await getPage(pageNumber, searchString))
     }
   }
 }
