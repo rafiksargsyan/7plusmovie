0eae019e056c5b5c11808aa6b73930d4a72cb3cb
Rafik <rafiksargsyan07@gmail.com>
2024-03-23 11:26:03 +0100

    Episode name normalization script (#16)
    * initial commit
    
    * Added to docker file
    
    * Fixed files with space in the name

---
+++ pirate-vm/media-toolbox/normalize-episode-names.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+REGEX=".*[s,S][0]*([1-9]+[0-9]*)[e,E][0]*([1-9]+[0-9]*).*"
+for f in *; do
+  extension="${f##*.}"
+  name="${f%.*}"
+  if [[ $name =~ $REGEX ]]
+  then
+    season="${BASH_REMATCH[1]}"
+    episode="${BASH_REMATCH[2]}"
+    touch ${episode}.${extension}
+    mv "$f" ${episode}.${extension}
+  fi
+done
