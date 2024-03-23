b770d54fdc655cfe46ac0540e5bcb250f88b0512
Rafik Sargsyan <rafiksargsyan07@gmail.com>
2024-03-23 12:54:45 +0100

    Added new pattern

--- pirate-vm/media-toolbox/normalize-episode-names.sh
+++ pirate-vm/media-toolbox/normalize-episode-names.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-REGEX=".*[s,S][0]*([1-9]+[0-9]*)[e,E][0]*([1-9]+[0-9]*).*"
+REGEX1=".*[s,S][0]*([1-9]+[0-9]*)[e,E][0]*([1-9]+[0-9]*).*"
+REGEX2="[0]*([1-9]+[0-9]*)\..*"
 for f in *; do
   extension="${f##*.}"
   name="${f%.*}"
-  if [[ $name =~ $REGEX ]]
+  if [[ $name =~ $REGEX1 ]]
   then
     season="${BASH_REMATCH[1]}"
     episode="${BASH_REMATCH[2]}"
+    touch ${episode}.${extension}
+    mv "$f" ${episode}.${extension}
+  elif [[ $name =~ $REGEX2 ]]
+  then
+    episode="${BASH_REMATCH[1]}"
     touch ${episode}.${extension}
     mv "$f" ${episode}.${extension}
   fi
