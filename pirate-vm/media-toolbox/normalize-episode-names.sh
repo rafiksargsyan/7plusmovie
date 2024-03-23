263524e6f44f89101a783b1c6204677beb7fe322
Rafik Sargsyan <rafiksargsyan07@gmail.com>
2024-03-23 13:59:41 +0100

    Added third regex

--- pirate-vm/media-toolbox/normalize-episode-names.sh
+++ pirate-vm/media-toolbox/normalize-episode-names.sh
@@ -2,6 +2,7 @@
 
 REGEX1=".*[s,S][0]*([1-9]+[0-9]*)[e,E][0]*([1-9]+[0-9]*).*"
 REGEX2="[0]*([1-9]+[0-9]*)\..*"
+REGEX3=".*[0]*([1-9]+[0-9]*)[x,X][0]*([1-9]+[0-9]*).*"
 for f in *; do
   extension="${f##*.}"
   name="${f%.*}"
@@ -14,6 +15,11 @@ for f in *; do
   elif [[ $name =~ $REGEX2 ]]
   then
     episode="${BASH_REMATCH[1]}"
+    touch ${episode}.${extension}
+    mv "$f" ${episode}.${extension}
+  elif [[ $name =~ $REGEX3 ]]
+  then
+    episode="${BASH_REMATCH[2]}"
     touch ${episode}.${extension}
     mv "$f" ${episode}.${extension}
   fi
