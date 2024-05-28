e8f83fb496da182c3548e1d3f29a68c213fb322f
Rafik Sargsyan <rafiksargsyan07@gmail.com>
2024-05-28 17:47:46 +0200

    Added torrent download handler basic api calls

--- services/release-indexer-context/src/core/app/torrentDownloadHandler.ts
+++ services/release-indexer-context/src/core/app/torrentDownloadHandler.ts
@@ -2,7 +2,6 @@ import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
 import { DynamoDB } from '@aws-sdk/client-dynamodb';
 import { MovieRepositoryInterface } from '../ports/MovieRepositoryInterface';
 import { MovieRepository } from '../../adapters/MovieRepository';
-import { handler as updateMovieReleaseCandidates } from './updateMovieReleaseCandidates';
 import { SecretsManager } from '@aws-sdk/client-secrets-manager';
 import axios from 'axios';
 
@@ -16,9 +15,9 @@ const docClient = DynamoDBDocument.from(new DynamoDB({}), translateConfig);
 const movieRepo: MovieRepositoryInterface = new MovieRepository(docClient);
 const secretsManager = new SecretsManager({});
 
-const qbittorrentApiBaseUrl = process.env.QBITTORENT_API_BASE_URL!;
-const qbittorrentBaseUrl = process.env.QBITTORENT_BASE_URL!;
-const qbittorrentUsername = process.env.QBITTORENT_USERNAME!;
+const qbittorrentApiBaseUrl = process.env.QBITTORRENT_API_BASE_URL!;
+const qbittorrentBaseUrl = process.env.QBITTORRENT_BASE_URL!;
+const qbittorrentUsername = process.env.QBITTORRENT_USERNAME!;
 const secretManagerSecretId = process.env.SECRET_MANAGER_SECRETS_ID!;
 
 const qbittorrentClient = axios.create({
@@ -31,6 +30,14 @@ export const handler = async (): Promise<void> => {
   const qbittorrentPassword = secret.QBITTORRENT_PASSWORD!;
   const loginResponse = (await qbittorrentClient.post('auth/login', `username=${qbittorrentUsername}&password=${qbittorrentPassword}`, {
     headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'Referer': `${qbittorrentBaseUrl}` }
-  })).data;
+  })).headers;
   console.log(JSON.stringify(loginResponse));
+  const setCookie = loginResponse['set-cookie'];
+  let sessionCookie;
+  if (setCookie != null) sessionCookie = setCookie[0];
+  sessionCookie = sessionCookie.substring(0, sessionCookie.indexOf(';'));
+  const logoutResponse = (await qbittorrentClient.post('auth/logout', null, {
+    headers: { 'Cookie': `${sessionCookie}`, 'Referer': `${qbittorrentBaseUrl}` }
+  })).status;
+  console.log(JSON.stringify(logoutResponse));
 };
