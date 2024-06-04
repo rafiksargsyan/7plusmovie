63550eb185512c0fc07aec2988e2a936879f3bd8
Rafik Sargsyan <rafiksargsyan07@gmail.com>
2024-06-04 07:07:57 +0200

    Fixed audio author resolution logic

--- services/release-indexer-context/src/core/domain/service/resolveAudioAuthor.ts
+++ services/release-indexer-context/src/core/domain/service/resolveAudioAuthor.ts
@@ -1,9 +1,10 @@
 import { Nullable } from "../../../Nullable";
+import { AudioAuthor } from "../value-object/AudioAuthor";
 import { TorrentTracker } from "../value-object/TorrentTracker";
 
 export function resolveAudioAuthor(title: Nullable<string>, tracker: Nullable<TorrentTracker>) {
   if (title == null) return null;  
-  if (title.toLowerCase().includes("hdrezka")) return this.HDREZKA;
-  if (title.toLowerCase().includes("tvshows")) return this.TVSHOWS;
-  if (title.toLowerCase().includes("lost film")) return this.LOSTFILM;
+  if (title.toLowerCase().includes("hdrezka")) return AudioAuthor.HDREZKA;
+  if (title.toLowerCase().includes("tvshows")) return AudioAuthor.TVSHOWS;
+  if (title.toLowerCase().includes("lost film")) return AudioAuthor.LOSTFILM;
 }
