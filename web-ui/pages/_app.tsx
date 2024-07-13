33af9852a7b73cd03569588de739ca9bd15d2e76
Rafik Sargsyan <rafiksargsyan07@gmail.com>
2024-07-13 12:32:24 +0200

    Switched to vidstack player

--- web-ui/pages/_app.tsx
+++ web-ui/pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router';
 import Script from 'next/script';
 import { useEffect, useState } from 'react';
-import '../components/plyr-overrides.css';
+import '../components/vidstack-overrides.css';
 import { Analytics } from '@vercel/analytics/react';
 import { hotjar } from 'react-hotjar';
 
