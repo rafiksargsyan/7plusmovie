93ff62d638bb427741bf5f0e2c1befd2b4d4eeed
Rafik Sargsyan <rafiksargsyan07@gmail.com>
2024-08-02 23:06:36 +0400

    Added better torrent client

---
+++ services/release-indexer-context/src/core/ports/ITorrentClient.ts
@@ -0,0 +1,40 @@
+import { Nullable } from "./utils"
+
+export interface ITorrentClient {
+  version(): Promise<string>
+  deleteTorrentById(id: string): void
+  getTorrentById(id: string): Promise<Nullable<TorrentInfo>>
+  getEstimatedFreeSpace(): Promise<number>
+  addTorrentByUrl(url: string, id: string, tags: Nullable<string[]>): Promise<TorrentInfo>
+  resumeTorrent(id: string): void
+  pauseTorrent(id: string): void
+  disableAllFiles(id: string): void
+  enableFile(id: string, fileId: number): void
+  addTag(id: string, tag: string): void
+  removeTag(id: string, tag: string): void
+}
+
+export interface TorrentInfo {
+  id: string;
+  infoHash: string;
+  addedOn: number; // epoch seconds
+  amountLeft: number; // bytes
+  isStalled: boolean;
+  files: { name: string, size: number, progress: number, index: number } []
+  eta: Nullable<number>; // seconds
+  tags: string[];
+}
+
+export class TorrentRuntimeError extends Error {
+  constructor(msg: string | undefined) {
+    super(msg)
+    this.name = 'TorrentRuntimeError'
+  }
+}
+
+export class TorrentError extends Error {
+  constructor(msg: string | undefined) {
+    super(msg)
+    this.name = 'TorrentError'
+  }
+}
