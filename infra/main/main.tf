f40aca800d43a82d7ab96189aa35a81c0edd067f
Rafik Sargsyan <rafiksargsyan07@gmail.com>
2024-08-13 17:41:36 +0400

    Delete objects after 7 days as we already store them in cloudflare and also have archive account

--- infra/main/main.tf
+++ infra/main/main.tf
@@ -16,10 +16,10 @@ resource "aws_s3_bucket_lifecycle_configuration" "media_assets_lifecycle_config"
   bucket = aws_s3_bucket.media_assets.bucket
 
   rule {
-    id = "intelligent-tiering"
+    id = "expire-after-one-week"
 
-    transition {
-        storage_class = "INTELLIGENT_TIERING"
+    expiration {
+      days = 7
     }
 
     status = "Enabled"
