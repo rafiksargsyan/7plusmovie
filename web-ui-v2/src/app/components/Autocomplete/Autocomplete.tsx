b38d4e7bb356aed1cac706b3bffc09e81a8b81f6
Rafik Sargsyan <rafiksargsyan07@gmail.com>
2025-01-19 21:46:05 +0400

    Autocomplete in progress

--- web-ui-v2/src/app/components/Autocomplete/Autocomplete.tsx
+++ web-ui-v2/src/app/components/Autocomplete/Autocomplete.tsx
@@ -1,19 +1,25 @@
 'use client'
-import { useState } from "react";
+import { useRef, useState } from "react";
 import styles from "./Autocomplete.module.css";
-import CancelRoundedIcon from '@mui/icons-material/CancelRounded';
+import ClearIcon from '@mui/icons-material/Clear';
+import IconButton from "@mui/material/IconButton";
+import grey from "@mui/material/colors/grey";
 
 interface AutocompleteProps {
   inputPlaceholder: string;
 }
 
 export function Autocomplete(props: AutocompleteProps) {
-  const [inputText, setInputText] = useState("");
+  const inputRef = useRef(null);
+  const clearButtonRef = useRef(null);
 
   return (
     <div className={styles.root}>
-      <input className={styles.input} type="text" placeholder={props.inputPlaceholder} value={inputText} onChange={(e) => setInputText(e.target.value)}/>
-      { inputText == "" || <button onClick={() => setInputText("")} > <CancelRoundedIcon /> </button> }
+      <input ref={inputRef} className={styles.input} type="text" placeholder={props.inputPlaceholder} onChange={(e) => { (clearButtonRef.current as any).style.display = (e.target.value == "" ? "none" : "flex") }}/>
+      <IconButton sx={{ "&:hover": { bgcolor: grey[900] }, display: "none", alignItems: "center" }}
+                  ref={clearButtonRef}
+                  size="small"
+                  onClick={() => { (inputRef.current as any).value = ""; (clearButtonRef.current as any).style.display = "none"; (inputRef.current as any).focus() }}><ClearIcon sx={{ color: grey[400] }} /></IconButton>
     </div>
   )
 }
