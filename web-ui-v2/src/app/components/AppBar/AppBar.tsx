dd68d9295a38ce5fe26545b313a46e5e4fe9a21a
Rafik Sargsyan <rafiksargsyan07@gmail.com>
2025-01-20 18:35:26 +0400

    AppBar in progress

--- web-ui-v2/src/app/components/AppBar/AppBar.tsx
+++ web-ui-v2/src/app/components/AppBar/AppBar.tsx
@@ -1,8 +1,19 @@
+import IconButton from "@mui/material/IconButton";
 import { Autocomplete } from "../Autocomplete/Autocomplete";
+import MenuIcon from '@mui/icons-material/Menu';
+import styles from './AppBar.module.css';
+import grey from "@mui/material/colors/grey";
 
 export function AppBar() {
   return (
-    <header>
+    <header className={styles.root}>
+      <IconButton
+        size="large"
+        edge="start"
+        sx={{ color: "white",  "&:hover": { bgcolor: grey[800] } }}
+      >
+        <MenuIcon />
+      </IconButton>
       <Autocomplete inputPlaceholder="Search" />
     </header>
   )
